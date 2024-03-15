62520852dbf88e28bbc43fdfdf6f0731b21f8519
deadrudolph <deadrudolph@gmail.com>
2024-03-15 06:55:23 -0400

    Initial commit

---
+++ app/src/main/kotlin/com/deadrudolph/template_mvvm/di/component/main/AppActivityComponent.kt
@@ -0,0 +1,8 @@
+package com.deadrudolph.template_mvvm.di.component.main
+
+import com.deadrudolph.template_mvvm.presentation.ui.activity.MainActivity
+
+internal interface AppActivityComponent {
+
+    fun inject(activity: MainActivity)
+}
